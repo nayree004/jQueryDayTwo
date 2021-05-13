02f59a8bd9963a3dcf8c420ec26e4c7bfe61f283
nayree004 <nayree.naynay3@gmail.com>
2021-05-12 19:52:36 -0600

    Created a document that displays array items that are in stock and toggles to a dark mode

---
+++ script.js
@@ -0,0 +1,88 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+function task(){
+    storeItems.forEach(function(object) {
+      if (object.inStock === true){
+        $("#appendToThis").append(`<div  class= "inStock">
+        <p>   $${object.price}</p>
+        <p>${object.name}</p>
+        <p>${object.details}   </p>
+      </div>
+      `);
+    };
+    });
+  }
+  console.log(task())
+
+const button = document.getElementById('clickHere');
+
+button.addEventListener('click', function() {
+$('.toddleThis').toggleClass('darkMode');
+});
